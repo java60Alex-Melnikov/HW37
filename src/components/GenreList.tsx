ec3ad5dcd4cf286d6269da4eee5747f995a92087
Millenium-Konig <sanyamelnikov097+@gmail.com>
2025-05-20 15:47:45 +0300

    added spinner for genres too

--- src/components/GenreList.tsx
+++ src/components/GenreList.tsx
@@ -1,13 +1,15 @@
-import { List, Button, Text, Stack, Avatar, HStack } from '@chakra-ui/react'
+import { List, Button, Text, Stack, Avatar, HStack, Spinner } from '@chakra-ui/react'
 import useGenre from '../hooks/useGenre';
 
 const GenreList = () => {
-    const { data, error } = useGenre();
+    const { data, error, isLoading } = useGenre();
     const genres = data;
     return (
             error ? (
-              <Text color={"red"} fontSize={"2rem"}>{error}</Text>
-            ) :
+                    <Text color={"red"} fontSize={"2rem"}>{error}</Text>
+                  ) : isLoading ? (
+                    <Spinner size="xl" boxSize="65px" borderWidth="10px" color="yellow.500" marginTop= "40%" marginLeft= "10%"/>
+                  ) :
         <Stack>
             <Text fontSize="2xl" fontWeight="bold" paddingLeft={2}>Genres</Text>
             <List.Root gap={2} padding={2}>
--- src/hooks/useGenre.ts
+++ src/hooks/useGenre.ts
@@ -1,6 +1,7 @@
 import { Genre } from "../model/fetch-genre-types";
 import useData from "./useData";
 
-export default function useGenre(): {data: Genre[], error: string} {
-    return useData<Genre>("/genres")
+export default function useGame(): { data: Genre[], error: string, isLoading: boolean } {
+  const { data, error, isLoading } = useData<Genre>("/genres");
+  return { data, error, isLoading };
 }
