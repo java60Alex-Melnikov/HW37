251ce8d9dbf393cea275180ddb7c71c7b1c1f105
Millenium-Konig <sanyamelnikov097+@gmail.com>
2025-05-20 15:48:42 +0300

    changed api key to my own

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -2,6 +2,6 @@ import axios from "axios";
 export default axios.create({
     baseURL: "https://api.rawg.io/api",
     params: {
-        key: '06308cfd74c447b4b6cfed15abe3de7a'
+        key: 'f632b77d910741ac8739d6e7673d02e5'
     }
 })
